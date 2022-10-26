601c4899ea050af3bc10bc24e38b1a76adfd14da
alvaroLZ96 <alvarost54@gmail.com>
2022-10-26 22:27:20 +0200

    estilos css

--- mini-proyecto-1/src/App.jsx
+++ mini-proyecto-1/src/App.jsx
@@ -6,17 +6,15 @@ const App = () => {
     <div className="App">
       <header className="header">
         <h1>The Music Library</h1>
-      </header>
-      <div>
         <nav>
           <NavLink to="">Home</NavLink>
           <NavLink to="music">Music</NavLink>
           <NavLink to="about">About</NavLink>
         </nav>
-        <main>
-          <Outlet />
-        </main>
-      </div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 };
--- mini-proyecto-1/src/pages/About.jsx
+++ mini-proyecto-1/src/pages/About.jsx
@@ -9,6 +9,7 @@ const About = () => {
         tanto de los últimos lanzamientos!
       </p>
       <img
+        id="vinyls"
         src="https://res.cloudinary.com/ddtufnl04/image/upload/v1666795217/Vinyl-Record-Transparent-Free-PNG_vkfyad.png"
         alt="Vinilos"
       />
